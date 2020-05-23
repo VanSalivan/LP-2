5f389a238ef001ec90542b1d5388584f52b57648
VanSalivan_NoteBook <myvansalivan@gmail.com>
2020-05-24 01:12:03 +0400

    feat:add header-menu

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,1 +1,5 @@
-@@include('webp.js')
+let userIcon = document.querySelector('.user-header__icon');
+userIcon.addEventListener('click', function (e) {
+    let userMenu = document.querySelector('.user-header__menu');
+    userMenu.classList.toggle("_active");
+})
