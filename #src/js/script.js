12e9e0f790a51ee3f201febd9b59a4cc2eb3a387
VanSalivan <myvansalivan@gmail.com>
2020-05-24 19:10:57 +0400

    feat: little fix header & burger

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,5 +1,30 @@
-let userIcon = document.querySelector('.user-header__icon');
-userIcon.addEventListener('click', function (e) {
-    let userMenu = document.querySelector('.user-header__menu');
+@@include('./app.js');
+//----------------------------------------------
+
+const USER_ICON = document.querySelector(".user-header__icon");
+const MOBILE_BACKGROUND = document.querySelector(".menu__body");
+const BURGER_BUTTON = document.querySelector(".burger-menu");
+
+USER_ICON.addEventListener('click', function (e) {
+
+    let userMenu = document.querySelector(".user-header__menu");
     userMenu.classList.toggle("_active");
-})
+});
+
+BURGER_BUTTON.addEventListener('click', (event) => {
+
+    MOBILE_BACKGROUND.classList.toggle("_active");
+
+    if (MOBILE_BACKGROUND.classList.value === "menu__body _active") {
+        BURGER_BUTTON.style.transform = 'rotate(90deg)';
+    } else {
+        BURGER_BUTTON.style.transform = 'rotate(0)'
+    };
+});
+
+document.addEventListener('click', function (e) {
+    if (!e.target.closest(".user-header")) {
+        let userMenu = document.querySelector(".user-header__menu");
+        userMenu.classList.remove("_active");
+    }
+});
